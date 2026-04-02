715dd65d3d86a762da0315c80b338be2c30f977a
PriyankaPatel01 <priyankarpatel192000@gmail.com>
2026-04-03 00:00:24 +0530

    Added imports

--- src/main/java/com/onerivet/ticket/controller/AgentController.java
+++ src/main/java/com/onerivet/ticket/controller/AgentController.java
@@ -1,5 +1,6 @@
 package com.onerivet.ticket.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
